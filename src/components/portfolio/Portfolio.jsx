27b22e1515b319583477b75b695bd22fa770d099
syrym-umbetov <umbetov.syrym@gmail.com>
2022-04-17 14:22:16 +0600

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PortfolioList from '../portfoliolist/PortfolioList';
 import './portfolio.scss';
 
+import {
+  contentPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  featuredPortfolio,
+} from './../../data';
+
 const Portfolio = () => {
-  return <div className='portfolio' id='portfolio'></div>;
+  const [selected, setSelected] = useState('featured');
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: 'featured',
+      title: 'Featured',
+    },
+    {
+      id: 'web',
+      title: 'Web App',
+    },
+    {
+      id: 'mobile',
+      title: 'Mobile App',
+    },
+    {
+      id: 'design',
+      title: 'Design',
+    },
+    {
+      id: 'content',
+      title: 'Content',
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case 'featured':
+        setData(featuredPortfolio);
+        break;
+      case 'web':
+        setData(webPortfolio);
+        break;
+      case 'mobile':
+        setData(mobilePortfolio);
+        break;
+      case 'design':
+        setData(designPortfolio);
+        break;
+      case 'content':
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
+  return (
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className='container'>
+        {data.map((data) => (
+          <div className='item'>
+            <img src={data.img} alt='' />
+            <h3>{data.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
