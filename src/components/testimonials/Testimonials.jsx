d1901ddace3e787f4fdfc058f1cb8e2cda0ab6af
syrym-umbetov <umbetov.syrym@gmail.com>
2022-04-17 15:55:09 +0600

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -34,14 +34,17 @@ const Testimonials = () => {
       <h1>Testimonials</h1>
       <div className='container'>
         {data.map((d) => (
-          <div>
+          <div className={d.featured ? 'card featured' : 'card'}>
             <div className='top'>
               <img src='assets/right-arrow.png' className='left' alt='' />
-              <img src={d.img} alt='' />
-              <img src={d.icon} alt='' />
+              <img className='user' src={d.img} alt='' />
+              <img className='right' src={d.icon} alt='' />
             </div>
-            <div className='center'></div>
-            <div className='bottom'></div>
+            <div className='center'>{d.desc}</div>
+            <div className='bottom'>
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
         ))}
       </div>
